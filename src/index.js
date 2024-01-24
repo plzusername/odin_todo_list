6b15bb04a67859ce2f15db8b198f66ddd37d8fe3
plzusername <MohamedFaris2210@gmail.com>
2024-01-24 20:25:34 +0300

    Complete filter test

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import {projectGenerator} from './modules/Logic/project-Generator.js'
-
+import {viewAll} from './modules/Logic/view-all-projects.js'
+const { isBefore, isAfter, add  } = require("date-fns");
 let a=new projectGenerator()
-let b=new projectGenerator()
-let c=new projectGenerator()
-let d=new projectGenerator()
-let e=new projectGenerator()
-let af=new projectGenerator()
-let aff=new projectGenerator()
-let afdf=new projectGenerator()
 
 
-a.addTask('Hi there', 'hi there', false, 'LOW', '12/12/2024')
-a.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
-b.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
-c.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
-a.tasks[0].changeProject(c)
-d.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
-c.removeTask(0)
-b.tasks[0].check()
+a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2028, 0, 20)}`)
+a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
+a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
+a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 29)}`)
 
-console.log([a,b,c,d,e,af,aff,afdf])
+console.log(viewAll.filterTasksBasedOnDays(4000))
+console.log(viewAll.projects)
