dbdb22656254d7255932592565ded16ee6587d77
plzusername <MohamedFaris2210@gmail.com>
2024-01-24 13:34:24 +0300

    Create a class to add projects

--- src/modules/Logic/project-Generator.js
+++ src/modules/Logic/project-Generator.js
@@ -1,8 +1,18 @@
+import {viewAll} from './view-all-projects.js'
+
 class projectGenerator{
     constructor(title = 'Project Title'){
         this.title=title
         this.tasks=[]
+        viewAll.projects.append(this)
+
+    }
+    
+    remove(){
+        this.tasks=[]
+        viewAll.projects.splice(this.id,1)
     }
 
-
 }
+
+export {projectGenerator}
