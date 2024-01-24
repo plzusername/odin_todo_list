8c9f189d3f37cee54968d2f2fdee723245600500
plzusername <MohamedFaris2210@gmail.com>
2024-01-24 13:57:23 +0300

    Create add method to add new tasks

--- src/modules/Logic/project-Generator.js
+++ src/modules/Logic/project-Generator.js
@@ -1,16 +1,21 @@
 import {viewAll} from './view-all-projects.js'
 
 class projectGenerator{
-    constructor(title = 'Project Title'){
+    constructor(title = `Project #${viewAll.projects.length + 1}`){
         this.title=title
         this.tasks=[]
-        viewAll.projects.append(this)
+        viewAll.projects.push(this)
+        this.id=viewAll.projects.length - 1
 
     }
     
     remove(){
         this.tasks=[]
         viewAll.projects.splice(this.id,1)
+    }
+
+    addTask(task){
+        this.tasks.push(task)
     }
 
 }
