9c7428ddfa4b245a31949b17600e2697dba4e74c
plzusername <MohamedFaris2210@gmail.com>
2024-01-24 18:37:37 +0300

    Complete testing of basic functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import {projectGenerator} from './modules/Logic/project-Generator.js'
 
-let projectTest = new projectGenerator()
+let a=new projectGenerator()
+let b=new projectGenerator()
+let c=new projectGenerator()
+let d=new projectGenerator()
+let e=new projectGenerator()
+let af=new projectGenerator()
+let aff=new projectGenerator()
+let afdf=new projectGenerator()
+
+
+a.addTask('Hi there', 'hi there', false, 'LOW', '12/12/2024')
+a.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
+b.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
+c.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
+a.tasks[0].changeProject(c)
+d.addTask('Hi there', 'hi there', true, 'LOW', '12/12/2024')
+c.removeTask(0)
+
+console.log([a,b,c,d,e,af,aff,afdf])
