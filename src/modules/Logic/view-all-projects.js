1cdf3586f7830ad4ecfba3155dc3c903c8bb0461
plzusername <MohamedFaris2210@gmail.com>
2024-01-24 20:24:09 +0300

    Add filter to filter projects based on day params

--- src/modules/Logic/view-all-projects.js
+++ src/modules/Logic/view-all-projects.js
@@ -1,5 +1,16 @@
+const { isBefore, isAfter, add  } = require("date-fns");
+
 class viewAll{
     static projects = []
+
+    static filterTasksBasedOnDays(daysIntoFuture) {
+        viewAll.projects.forEach(project => {
+            project.tasks = project.tasks.filter(task => 
+              isBefore(task.due_date, add(new Date(), { days: daysIntoFuture })) &&
+              isAfter(task.due_date, new Date())
+            );
+          });
+    }
 }
 
 export {viewAll}
