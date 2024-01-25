a955f9593e536dbeacfb128e25763c4004f3cddc
plzusername <MohamedFaris2210@gmail.com>
2024-01-25 19:21:25 +0300

    Add id to generated notes

--- src/modules/Logic/notes-generator.js
+++ src/modules/Logic/notes-generator.js
@@ -1,7 +1,10 @@
+import { notesSection } from "./notes-section"
+
 class notesGenerator{
     constructor(title, description){
         this.title=title
         this.description=description
+        this.id = notesSection.notes.length
     }
 
     editNote(newTitle, newDescription){
