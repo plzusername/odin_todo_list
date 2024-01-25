c5c552a0e4057eb94afa86125db49ce86712c9d9
plzusername <MohamedFaris2210@gmail.com>
2024-01-25 19:22:18 +0300

    Send deleted notes to trash section

--- src/modules/Logic/notes-section.js
+++ src/modules/Logic/notes-section.js
@@ -1,9 +1,16 @@
+import { trashSection } from "./trash-section"
+
 class notesSection{
     static notes=[]
 
-    static addTrashedTasks(task){
-        trashSection.push(task)
+    static addNote(note){
+        notesSection.notes.push(note)
+    }
+
+    static removeNote(note){
+        notesSection.notes.splice(note.id,1)
+        trashSection.addTrashedTask(note)
     }
 }
 
-export {trashSection}
+export {notesSection}
