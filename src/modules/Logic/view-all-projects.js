e153dde76a6b6425a1eaa7b706df2b211a6390fb
plzusername <MohamedFaris2210@gmail.com>
2024-01-26 15:20:04 +0300

    Copy array for dateFiltering,filter empty projects

--- src/modules/Logic/view-all-projects.js
+++ src/modules/Logic/view-all-projects.js
@@ -1,15 +1,21 @@
 const { isBefore, isAfter, add  } = require("date-fns");
-
+const { cloneDeep } = require('lodash')
 class viewAll{
     static projects = []
 
     static filterTasksBasedOnDays(daysIntoFuture) {
-        viewAll.projects.forEach(project => {
+        let toBeFilteredArray=cloneDeep(viewAll.projects)
+
+        toBeFilteredArray.forEach(project => {
             project.tasks = project.tasks.filter(task => 
               isBefore(task.due_date, add(new Date(), { days: daysIntoFuture })) &&
               isAfter(task.due_date, new Date())
             );
         });
+
+        toBeFilteredArray = toBeFilteredArray.filter(project => project.tasks.length != 0)
+
+        return toBeFilteredArray
     }
 }
 
