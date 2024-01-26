c08018799fb11aeaf2e3e2d3684ed0278b64f93d
plzusername <MohamedFaris2210@gmail.com>
2024-01-26 14:43:58 +0300

    Comlplete all Logic testing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import {projectGenerator} from './modules/Logic/project-Generator.js'
 import {viewAll} from './modules/Logic/view-all-projects.js'
+import { trashSection } from './modules/Logic/trash-section.js'
+import { notesSection } from './modules/Logic/notes-section.js'
+import { notesGenerator } from './modules/Logic/notes-generator.js'
+
 let a=new projectGenerator()
 let b=new projectGenerator()
 let c=new projectGenerator()
 
 
 a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2028, 0, 20)}`)
-a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-a.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 29)}`)
 b.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2028, 0, 20)}`)
-b.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-b.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-b.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 29)}`)
 c.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2028, 0, 20)}`)
-c.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-c.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 20)}`)
-c.addTask('Hi there', 'hi there', true, 'LOW', `${new Date(2024, 0, 29)}`)
 
+notesSection.addNote('health plans', 'plans to get healthy')
 
-console.log(viewAll.filterTasksBasedOnDays(10))
-console.log(viewAll.projects)
+notesSection.removeNote(notesSection.notes[0])
+console.log(notesSection.notes)
+trashSection.restoreFromTrash(trashSection.trashedItems[0])
+console.log(trashSection.trashedItems)
+console.log(notesSection.notes)
