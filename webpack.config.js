2fab35b2aa05b41dbd3d09e38d15aeaaec727abe
plzusername <MohamedFaris2210@gmail.com>
2024-01-26 17:19:57 +0300

    Allow webpack to handle font files

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
    module:{
     rules:[
             {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for processing CSS
             },
