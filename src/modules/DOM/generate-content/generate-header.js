14d5c64b34ab0332ba4b4fd42bbd66ab80189773
plzusername <MohamedFaris2210@gmail.com>
2024-01-26 17:20:53 +0300

    Fill header Creation code,export it as a variable

--- src/modules/DOM/generate-content/generate-header.js
+++ src/modules/DOM/generate-content/generate-header.js
@@ -1,8 +1,17 @@
 import { createElement } from '../utility/createElement.js'
 
 function createHeaderSection(){
-    
+    const todoLogoImage=createElement({class:'todo-logo-image fa-regular fa-circle-check'}, 'i', '',[])
+    const todoLogoHeader=createElement({class:'todo-logo-header'}, 'h1', 'TODO',[])
 
-    const header_section=createElement({class:'header-section'}, 'div', '', [])
+    const logoSection=createElement({class:'logo-section'}, 'div', '',[todoLogoImage,todoLogoHeader])
+    const signUpButton=createElement({class:'sign-up-button'}, 'button', 'Sign Up',[])
 
+    const header_section=createElement({class:'header-section'}, 'div', '', [logoSection,signUpButton])
+
+    return header_section
 }
+
+let header = createHeaderSection()
+
+export {header}
