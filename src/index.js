674d874248f712abaad59a6128d9333e9c9f61dc
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 23:25:17 +0300

    Test date filtering with negative inputs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import { header } from './modules/DOM/generate-content/generate-header.js'
 import { footer } from './modules/DOM/generate-content/generate-footer.js'
+import { sideBar } from './modules/DOM/generate-content/generate-sideBar.js';
+import { projectGenerator } from './modules/Logic/project-Generator.js';
+import { viewAll } from './modules/Logic/view-all-projects.js';
 
 document.body.appendChild(header)
+document.body.appendChild(sideBar)
 document.body.appendChild(footer)
+
+let a=new projectGenerator()
+let b=new projectGenerator()
+let c=new projectGenerator()
+
+a.addTask('title', 'description', true, 'LOW',  `${new Date(2023,9,10)}`)
+b.addTask('title', 'description', true, 'LOW',  `${new Date(2023,9,10)}`)
+c.addTask('title', 'description', true, 'LOW',  `${new Date(2025,9,10)}`)
+
+console.log(viewAll.filterTasksBasedOnDays(-3000))
+
+console.log(viewAll.projects)
