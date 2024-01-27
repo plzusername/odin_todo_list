aa7e5838a9afeef482608f09271f2f088faec522
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 14:33:47 +0300

    Fix footer github icon

--- src/modules/DOM/generate-content/generate-footer.js
+++ src/modules/DOM/generate-content/generate-footer.js
@@ -1,7 +1,7 @@
 import { createElement } from '../utility/createElement.js'
 
 function createFooterSection(){
-    const gtihubLogo=createElement({class:'github-logo fa-regular fa-github'}, 'i', '', [])
+    const gtihubLogo=createElement({class:'github-logo fa-brands fa-github'}, 'i', '', [])
     const githubUsername=createElement({class:'github-username'}, 'p', 'MOFRIS', [])
 
     const linkSection=createElement({class:'link-section'}, 'a', '', [gtihubLogo, githubUsername])
