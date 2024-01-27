6c9da86005e2b9b646e8c45d66421349e3e9225b
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 23:02:16 +0300

    Make date filtering work with negative inputs

--- src/modules/Logic/view-all-projects.js
+++ src/modules/Logic/view-all-projects.js
@@ -1,3 +1,26 @@
+// const { isBefore, isAfter, add  } = require("date-fns");
+// const { cloneDeep } = require('lodash')
+// class viewAll{
+//     static projects = []
+
+//     static filterTasksBasedOnDays(daysIntoFuture) {
+//         let toBeFilteredArray=cloneDeep(viewAll.projects)
+
+//         toBeFilteredArray.forEach(project => {
+//             project.tasks = project.tasks.filter(task => 
+//               isBefore(task.due_date, add(new Date(), { days: daysIntoFuture })) &&
+//               isAfter(task.due_date, new Date())
+//             );
+//         });
+
+//         toBeFilteredArray = toBeFilteredArray.filter(project => project.tasks.length != 0)
+
+//         return toBeFilteredArray
+//     }
+// }
+
+// export {viewAll}
+
 const { isBefore, isAfter, add  } = require("date-fns");
 const { cloneDeep } = require('lodash')
 class viewAll{
@@ -23,4 +46,4 @@ class viewAll{
 
 
 
-// export {viewAll}
+export {viewAll}
