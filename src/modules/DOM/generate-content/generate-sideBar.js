418ffa777fab9d51d1c272a9369a1069f616a0a4
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 23:26:50 +0300

    Load the rest of the sidebars content

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -5,8 +5,12 @@ function createSideBarSection(){
 
     const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter')]
 
+    const project_input=createElement({class:'add-project-input'}, 'input', '', [])
+
+    const add_project_section=createElement({class:'add-project-label'}, 'label', 'Add project', [project_input])
+
     const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [...home_section_filters])
-    const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [])
+    const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [add_project_section])
 
     const sideBar_section=createElement({class:'sideBar-section'}, 'div', '', [home_section, projects_section])
 
