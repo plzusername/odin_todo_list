ea03e654e935966ad42ab84c7608a1e25c4cecc5
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 14:31:10 +0300

    Generate footer section

---
+++ src/modules/DOM/generate-content/generate-footer.js
@@ -0,0 +1,16 @@
+import { createElement } from '../utility/createElement.js'
+
+function createFooterSection(){
+    const gtihubLogo=createElement({class:'github-logo fa-regular fa-github'}, 'i', '', [])
+    const githubUsername=createElement({class:'github-username'}, 'p', 'MOFRIS', [])
+
+    const linkSection=createElement({class:'link-section'}, 'a', '', [gtihubLogo, githubUsername])
+
+    const footer_section=createElement({class:'footer-section'}, 'div', '', [linkSection])
+
+    return footer_section
+}
+
+let footer = createFooterSection()
+
+export {footer}
