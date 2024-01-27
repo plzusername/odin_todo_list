adefb129cd99ac769950bddf3c30834ead9a69de
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 22:41:49 +0300

    Add home section filters

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -1,8 +1,11 @@
 import { createElement } from '../utility/createElement.js'
+import { generateFilter } from '../DOM-functions/generate-filter.js'
 
 function createSideBarSection(){
 
-    const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [])
+    const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter')]
+
+    const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [...home_section_filters])
     const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [])
 
     const sideBar_section=createElement({class:'sideBar-section'}, 'div', '', [home_section, projects_section])
