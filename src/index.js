a4af47e4eab7741caa46ce8ea2d6f2e84b2e6f97
plzusername <MohamedFaris2210@gmail.com>
2024-01-27 14:31:32 +0300

    Append footer section to body

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import { header } from './modules/DOM/generate-content/generate-header.js'
+import { footer } from './modules/DOM/generate-content/generate-footer.js'
 
 document.body.appendChild(header)
+document.body.appendChild(footer)
