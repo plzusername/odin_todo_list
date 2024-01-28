b43884c95846d35951c81052fc27fc548d44fad5
plzusername <MohamedFaris2210@gmail.com>
2024-01-28 19:44:46 +0300

    Add headers to projects and home section

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -3,14 +3,17 @@ import { generateFilter } from '../DOM-functions/generate-filter.js'
 
 function createSideBarSection(){
 
+    const home_section_header=createElement({class:'home-section-header'}, 'h1', 'Home', [])
+    const project_section_header=createElement({class:'project-section-header'}, 'h1', 'Projects', [])
+
     const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter'),generateFilter('Notes', 'notes-section')]
 
     const project_input=createElement({class:'add-project-input'}, 'input', '', [])
 
     const add_project_section=createElement({class:'add-project-label'}, 'label', 'Add project', [project_input])
 
-    const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [...home_section_filters])
-    const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [add_project_section])
+    const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [home_section_header,...home_section_filters])
+    const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [project_section_header,add_project_section])
 
     const sideBar_section=createElement({class:'sideBar-section'}, 'div', '', [home_section, projects_section])
 
