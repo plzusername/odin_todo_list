fff32d4db6c7c1a2d347ab3a0b03241fe644c12e
plzusername <MohamedFaris2210@gmail.com>
2024-01-28 19:40:48 +0300

    Change icon

--- src/modules/DOM/DOM-functions/generate-filter.js
+++ src/modules/DOM/DOM-functions/generate-filter.js
@@ -2,7 +2,7 @@ import { createElement } from '../utility/createElement.js'
 
 function generateFilter(filterSectionName,extraClasses){
     const filterName=createElement({class:`filter-name`}, 'span', filterSectionName, [])
-    const filterIcon=createElement({class:`filter-icon fa-regular fa-greater-than`}, 'i', '', [])
+    const filterIcon=createElement({class:`filter-icon fa-solid fa-chevron-right`}, 'i', '', [])
 
     const filter=createElement({class:`filter-container ${extraClasses}`}, 'div', '', [filterIcon, filterName])
 
