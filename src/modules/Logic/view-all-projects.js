4f8f34052356407e31b8ee81b43fcfa9f6a9ae50
plzusername <MohamedFaris2210@gmail.com>
2024-01-28 14:53:33 +0300

    Remove commented out section

--- src/modules/Logic/view-all-projects.js
+++ src/modules/Logic/view-all-projects.js
@@ -1,26 +1,3 @@
-// const { isBefore, isAfter, add  } = require("date-fns");
-// const { cloneDeep } = require('lodash')
-// class viewAll{
-//     static projects = []
-
-//     static filterTasksBasedOnDays(daysIntoFuture) {
-//         let toBeFilteredArray=cloneDeep(viewAll.projects)
-
-//         toBeFilteredArray.forEach(project => {
-//             project.tasks = project.tasks.filter(task => 
-//               isBefore(task.due_date, add(new Date(), { days: daysIntoFuture })) &&
-//               isAfter(task.due_date, new Date())
-//             );
-//         });
-
-//         toBeFilteredArray = toBeFilteredArray.filter(project => project.tasks.length != 0)
-
-//         return toBeFilteredArray
-//     }
-// }
-
-// export {viewAll}
-
 const { isBefore, isAfter, add  } = require("date-fns");
 const { cloneDeep } = require('lodash')
 class viewAll{
