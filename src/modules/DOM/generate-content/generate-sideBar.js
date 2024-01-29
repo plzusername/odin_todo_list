94d39d459ddfe47dffaefa41e1d36b8c9ed2d7e3
plzusername <MohamedFaris2210@gmail.com>
2024-01-29 13:59:42 +0300

    Add plus icon to add projects

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -9,8 +9,9 @@ function createSideBarSection(){
     const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter'),generateFilter('Notes', 'notes-section')]
 
     const project_input=createElement({class:'add-project-input'}, 'input', '', [])
+    const add_icon=createElement({class:'project-add-icon fa-solid fa-plus'}, 'i', '',[])
 
-    const add_project_section=createElement({class:'add-project-label'}, 'label', 'Add project', [project_input])
+    const add_project_section=createElement({class:'add-project-label'}, 'label', 'Add project', [project_input,add_icon])
 
     const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [home_section_header,...home_section_filters])
     const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [project_section_header,add_project_section])
