53c28c7ee00f5bca541887a2511e68da5aa22367
plzusername <MohamedFaris2210@gmail.com>
2024-01-29 14:06:03 +0300

    Change addProjectSection semantics for positioning

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -8,10 +8,13 @@ function createSideBarSection(){
 
     const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter'),generateFilter('Notes', 'notes-section')]
 
-    const project_input=createElement({class:'add-project-input'}, 'input', '', [])
+    const project_add_label=createElement({class:'add-project-input', id:'project_input'}, 'input', '', [])
+    const project_add_input=createElement({class:'add-project-input', for:'project_input'}, 'label', 'Add Project', [])
+
+    const project_input=createElement({class:'add-project-input'}, 'div', '', [project_add_label,project_add_input])
     const add_icon=createElement({class:'project-add-icon fa-solid fa-plus'}, 'i', '',[])
 
-    const add_project_section=createElement({class:'add-project-label'}, 'label', 'Add project', [project_input,add_icon])
+    const add_project_section=createElement({class:'add-project-label'}, 'div', '', [project_input,add_icon])
 
     const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [home_section_header,...home_section_filters])
     const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [project_section_header,add_project_section])
