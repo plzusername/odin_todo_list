ccedca97642eaae5151987c622e88b106afbf69d
plzusername <MohamedFaris2210@gmail.com>
2024-01-29 19:38:37 +0300

    MAke sidebar generation use input adder function

--- src/modules/DOM/generate-content/generate-sideBar.js
+++ src/modules/DOM/generate-content/generate-sideBar.js
@@ -1,5 +1,6 @@
 import { createElement } from '../utility/createElement.js'
 import { generateFilter } from '../DOM-functions/generate-filter.js'
+import { createInputAdder } from '../DOM-functions/create-input-adder.js'
 
 function createSideBarSection(){
 
@@ -8,13 +9,7 @@ function createSideBarSection(){
 
     const home_section_filters=[generateFilter('View All', 'view-all-filter'),generateFilter('Trash', 'trash-filter'),generateFilter('Time filter', 'time-filter'),generateFilter('Calendar', 'calendar-filter'),generateFilter('Notes', 'notes-section')]
 
-    const project_add_label=createElement({class:'add-project-label', for:'project_input'}, 'label', 'Add Project', [])
-    const project_add_input=createElement({class:'add-project-input', id:'project_input'}, 'input', '', [])
-
-    const project_input=createElement({class:'add-project-input-section'}, 'div', '', [project_add_label,project_add_input])
-    const add_icon=createElement({class:'project-add-icon fa-solid fa-plus'}, 'i', '',[])
-
-    const add_project_section=createElement({class:'add-project-section'}, 'div', '', [project_input,add_icon])
+    const add_project_section=createInputAdder('Add project')
 
     const home_section=createElement({class:'sideBar-home-section'}, 'div', '', [home_section_header,...home_section_filters])
     const projects_section=createElement({class:'sideBar-projects-section'}, 'div', '', [project_section_header,add_project_section])
