0436f852c498f0505e75127093722f3f802e4124
plzusername <MohamedFaris2210@gmail.com>
2024-01-30 19:43:22 +0300

    Store note and task forms in variables

--- src/modules/DOM/generate-content/generate-content-section.js
+++ src/modules/DOM/generate-content/generate-content-section.js
@@ -2,13 +2,17 @@ import { createElement } from '../utility/createElement.js'
 import { TaskForm } from '../DOM-functions/create-task-forms.js'
 import { NoteForm } from '../DOM-functions/create-note-forms.js'
 
-function createContentSection(){
+const noteForm=new NoteForm()
+const taskForm=new TaskForm()
 
-    const content_section=createElement({class:'content-section'}, 'div', '', [NoteForm.createForm(), TaskForm.createForm()])
+function createContentSection(){
+    
+
+    const content_section=createElement({class:'content-section'}, 'div', '', [noteForm.createForm(), taskForm.createForm()])
 
     return content_section
 }
 
 let content = createContentSection()
 
-export {content}
+export {content, noteForm, taskForm}
