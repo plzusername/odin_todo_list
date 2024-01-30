7ff7b290a4b0d475c7c225e917bf411376a7e00a
plzusername <MohamedFaris2210@gmail.com>
2024-01-30 19:42:44 +0300

    Return created element to be appended

--- src/modules/DOM/DOM-functions/generate-DOM-project.js
+++ src/modules/DOM/DOM-functions/generate-DOM-project.js
@@ -3,17 +3,13 @@ import { createInputAdder } from "./create-input-adder";
 import { viewAll } from '../../Logic/view-all-projects'
 
 function projectDomify(project){
-    const content=document.querySelector('.content-section')
-
     const projectName=createElement({class:'project-header'} ,'h1', project.title)
 
     const taskAdder=createInputAdder('Add task')
 
     const projectContainer=createElement({class:'project-container'}, 'div', '', [projectName,taskAdder])
 
-    content.appendChild(projectContainer)
-
-    viewAll.addProject(project)
+    return projectContainer
 
 }
 
