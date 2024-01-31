43508423fa3471132c2a6b9836304f8bbd883a98
plzusername <MohamedFaris2210@gmail.com>
2024-01-31 14:21:36 +0300

    Make generated html tasks

--- src/modules/DOM/DOM-functions/create-task-DOM.js
+++ src/modules/DOM/DOM-functions/create-task-DOM.js
@@ -1,15 +1,22 @@
 import { createElement } from "../utility/createElement";
 
 function createTask(task){
+    const titleText=document.createTextNode(task.title)
+    const descriptionText=document.createTextNode(task.description)
 
-    const check=createElement({class:'task-check', type:'checkbox', id:`${task.parent_project.tasks.length} ${task.parent_project.id}`}, 'input', '', [])
-    const taskTitle=createElement({class:'task-title', for:`${task.parent_project.tasks.length} ${task.parent_project.id}`}, 'p', '', [])
-    const taskDescription=createElement({class:'task-description'}, 'p',  '', [])
+    const check=createElement({class:'task-check', type:'checkbox'}, 'input', '', [])
+    const taskTitle=createElement({class:'task-title'}, 'p', '', [titleText])
+    const taskDescription=createElement({class:'task-description'}, 'p',  '', [descriptionText])
     const taskDateInput=createElement({class:'task-date-input', type:'date'}, 'input',  '', [])
     const taskDelete=createElement({class:'task-delete-icon, fa-solid fa-trash-can'}, 'i',  '', [])
     const taskEdit=createElement({class:'task-edit-icon fa-regular fa-pen-to-square'}, 'i',  '', [])
 
-    const taskContainer=createElement({class:'task-container'}, 'div', '', [check,taskTitle,taskDescription,taskDateInput,taskDelete,taskEdit])
+    const taskText=createElement({class:'task-text-container'}, 'div', '', [taskTitle, taskDescription])
+
+    const leftSide=createElement({class:'task-left-side'}, 'div', '', [check, taskText])
+    const rightSide=createElement({class:'task-right-side'}, 'div', '', [taskDateInput, taskDelete, taskEdit])
+
+    const taskContainer=createElement({class:'task-container'}, 'div', '', [leftSide,rightSide])
 
     return taskContainer
 
