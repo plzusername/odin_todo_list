9c4cf194d5a9608a354ffabd44dc4c0c649aaa90
plzusername <MohamedFaris2210@gmail.com>
2024-02-01 20:14:09 +0300

    Add function to save Storage's state

--- src/modules/Storage/storage-utils/save-storage.js
+++ src/modules/Storage/storage-utils/save-storage.js
@@ -1,5 +1,6 @@
 import { Storage } from "../main-Storage";
+const CircularJSON = require('circular-json')
 
 export function saveStorage(){
-    localStorage.setItem('Storage', JSON.stringify(Storage))
+    localStorage.setItem('Storage', CircularJSON.stringify(Storage))
 }
