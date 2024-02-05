b31384e8521beef48e870f2006ea6c2a293169d9
plzusername <MohamedFaris2210@gmail.com>
2024-02-05 16:22:22 +0300

    Delete storage function

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import { projectGenerator } from './modules/Logic/project-Generator.js';
 import { viewAll } from './modules/Logic/view-all-projects.js';
 import { content } from './modules/DOM/generate-content/generate-content-section.js';
 import '../src/modules/Styles/main.css'
-import { mainStorage } from './modules/Storage/main-Storage.js';
 
 document.body.appendChild(header)
 document.body.appendChild(sideBar)
 document.body.appendChild(content)
 document.body.appendChild(footer)
 
-
-
-mainStorage()
+// renderSotrage()
