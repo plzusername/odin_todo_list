95737162c3fc32637e1074e9d6ae1d58d1d80d34
plzusername <MohamedFaris2210@gmail.com>
2024-02-05 16:39:14 +0300

    Export storage variable

--- src/modules/Storage/storage-utils/save-storage.js
+++ src/modules/Storage/storage-utils/save-storage.js
@@ -1,6 +1,13 @@
-import { Storage } from "../main-Storage";
 const CircularJSON = require('circular-json')
 
 export function saveStorage(){
     localStorage.setItem('Storage', CircularJSON.stringify(Storage))
+}
+
+export const Storage = {
+    projects: [],
+    notes: [],
+    trash: [],
+    calendar: {},
+    tempDateFilter: []
 }
