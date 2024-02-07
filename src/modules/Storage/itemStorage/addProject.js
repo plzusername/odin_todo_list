66fb5f59af82d6fb0bcc444be18589cb318d802f
plzusername <MohamedFaris2210@gmail.com>
2024-02-07 16:12:13 +0300

    Make function to edit storage variable

--- src/modules/Storage/itemStorage/addProject.js
+++ src/modules/Storage/itemStorage/addProject.js
@@ -2,16 +2,31 @@ import { viewAll } from '../../Logic/view-all-projects'
 import { projectGenerator } from '../../Logic/project-Generator'
 import { content } from '../../DOM/generate-content/generate-content-section'
 import { projectDomify } from '../../DOM/DOM-functions/generate-DOM-project'
+import { Storage, saveStorage } from '../storage-utils/save-storage'
 
+let generatedProject
+
+function addProjectToStorage(){
+    if (typeof Storage == 'string'){
+        let parsedStorage = JSON.parse(Storage)
+        parsedStorage.projects.push(generatedProject)
+        return parsedStorage
+    }
+    Storage.projects.push(generatedProject)
+    return Storage
+
+}
 
 function addProjet(){
-    const Storage=localStorage.getItem('Storage')
-    const generatedProject = new projectGenerator()
+    generatedProject = new projectGenerator()
 
-    Storage.projects.push(generatedProject)
     viewAll.addProject(generatedProject)
 
     content.appendChild(projectDomify(generatedProject))
+
+    saveStorage(addProjectToStorage)
+
+    console.log(localStorage.getItem('Storage'))
 
 }
 
