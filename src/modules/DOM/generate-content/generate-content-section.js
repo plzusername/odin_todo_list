3e967ab23058b63f91db1d15fd9258b8b92f6f52
plzusername <MohamedFaris2210@gmail.com>
2024-02-07 20:21:24 +0300

    Append independant forms to content section

--- src/modules/DOM/generate-content/generate-content-section.js
+++ src/modules/DOM/generate-content/generate-content-section.js
@@ -1,18 +1,15 @@
 import { createElement } from '../utility/createElement.js'
-import { TaskForm } from '../DOM-functions/create-task-forms.js'
-import { NoteForm } from '../DOM-functions/create-note-forms.js'
-
-const noteForm=new NoteForm()
-const taskForm=new TaskForm()
+import { createEditTaskForm, createGenerateTaskForm } from '../DOM-functions/create-task-forms.js'
+import { createEditNoteForm, createGenerateNoteForm } from '../DOM-functions/create-note-forms.js'
 
 function createContentSection(){
     
 
-    const content_section=createElement({class:'content-section'}, 'div', '', [noteForm.createForm(), taskForm.createForm()])
+    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm, createEditTaskForm, createGenerateNoteForm, createGenerateTaskForm])
 
     return content_section
 }
 
 let content = createContentSection()
 
-export {content, noteForm, taskForm}
+export {content}
