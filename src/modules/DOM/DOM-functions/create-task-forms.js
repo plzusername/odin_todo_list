501f0e16cd2dbfe4b822ff73fc5274bab11aab20
plzusername <MohamedFaris2210@gmail.com>
2024-02-07 20:18:02 +0300

    Remove class in favour of more seperated functions

--- src/modules/DOM/DOM-functions/create-task-forms.js
+++ src/modules/DOM/DOM-functions/create-task-forms.js
@@ -1,49 +1,59 @@
-import { createElement } from "../utility/createElement";
+import { createElement } from "../utility/createElement"
 
-class TaskForm{
+function createGenerateTaskForm(){
+    const formHeader=createElement({class:'task-form-header'}, 'h1', `task`, [])
 
-    createForm(){
-        const formHeader=createElement({class:'task-form-header'}, 'h1', `task`, [])
+    const titleInput=createElement({class:'title-input-task'}, 'input', '', [])
+    const titleInputSection=createElement({class:'title-input-task-section'}, 'label', 'Title:', [titleInput])
 
-        const titleInput=createElement({class:'title-input-task'}, 'input', '', [])
-        const titleInputSection=createElement({class:'title-input-task-section'}, 'label', 'Title:', [titleInput])
+    const descriptionInput=createElement({class:'description-input-task'}, 'input', '', [])
+    const descriptionInputSection=createElement({class:'description-input-task-section'}, 'label', 'Description:', [descriptionInput])
+
+    const priorityInput=createElement({class:'priority-input-task'}, 'input', '', [])
+    const priorityInputSection=createElement({class:'priority-input-task-section'}, 'label', 'Priority:', [priorityInput])
+
+    const dueDateInput=createElement({class:'due-date-input-task'}, 'input', '', [])
+    const dueDateInputSection=createElement({class:'due-date-input-task-section'}, 'label', 'Due Date:', [dueDateInput])
+
+    const submitButton=createElement({class:'task-submit-button'} ,'button', `task`)
+
+
+    const taskForm=createElement({class:`task-form`}, 'div', '', [formHeader,titleInputSection,descriptionInputSection,priorityInputSection,dueDateInputSection,submitButton])
     
-        const descriptionInput=createElement({class:'description-input-task'}, 'input', '', [])
-        const descriptionInputSection=createElement({class:'description-input-task-section'}, 'label', 'Description:', [descriptionInput])
-    
-        const priorityInput=createElement({class:'priority-input-task'}, 'input', '', [])
-        const priorityInputSection=createElement({class:'priority-input-task-section'}, 'label', 'Priority:', [priorityInput])
-    
-        const dueDateInput=createElement({class:'due-date-input-task'}, 'input', '', [])
-        const dueDateInputSection=createElement({class:'due-date-input-task-section'}, 'label', 'Due Date:', [dueDateInput])
-    
-        const submitButton=createElement({class:'task-submit-button'} ,'button', `task`)
-    
-    
-        const taskForm=createElement({class:`task-form`}, 'div', '', [formHeader,titleInputSection,descriptionInputSection,priorityInputSection,dueDateInputSection,submitButton])
-    
-        return taskForm
-    
-    }
+    formHeader.textContent = 'Create form'
 
-    editorizeForm(){
-        const formHeader=document.querySelector('.task-form-header')
-        const submitButton=document.querySelector('.task-submit-button')
+    submitButton.addEventListener('click' , /*Create task with DOM logic and storage*/)
 
-        formHeader.textContent = 'Edit form'
+    return taskForm
 
-        submitButton.addEventListener('click' , /*Edit task with DOM logic and storage*/)
-    }
-
-    creatizeForm(){
-        const formHeader=document.querySelector('.task-form-header')
-        const submitButton=document.querySelector('.task-submit-button')
-
-        formHeader.textContent = 'Create form'
-
-        submitButton.addEventListener('click' , /*Create task with DOM logic and storage*/)
-
-    }
 }
 
-export {TaskForm}
+function createEditTaskForm(){
+    const formHeader=createElement({class:'task-form-header'}, 'h1', `task`, [])
+
+    const titleInput=createElement({class:'title-input-task'}, 'input', '', [])
+    const titleInputSection=createElement({class:'title-input-task-section'}, 'label', 'Title:', [titleInput])
+
+    const descriptionInput=createElement({class:'description-input-task'}, 'input', '', [])
+    const descriptionInputSection=createElement({class:'description-input-task-section'}, 'label', 'Description:', [descriptionInput])
+
+    const priorityInput=createElement({class:'priority-input-task'}, 'input', '', [])
+    const priorityInputSection=createElement({class:'priority-input-task-section'}, 'label', 'Priority:', [priorityInput])
+
+    const dueDateInput=createElement({class:'due-date-input-task'}, 'input', '', [])
+    const dueDateInputSection=createElement({class:'due-date-input-task-section'}, 'label', 'Due Date:', [dueDateInput])
+
+    const submitButton=createElement({class:'task-submit-button'} ,'button', `task`)
+
+
+    const taskForm=createElement({class:`task-form`}, 'div', '', [formHeader,titleInputSection,descriptionInputSection,priorityInputSection,dueDateInputSection,submitButton])
+
+    formHeader.textContent = 'Edit form'
+
+    submitButton.addEventListener('click' , /*Create task with DOM logic and storage*/)
+
+    return taskForm
+
+}
+
+export {createEditTaskForm,createGenerateTaskForm}
