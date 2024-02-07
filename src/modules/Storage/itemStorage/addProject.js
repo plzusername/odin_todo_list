a5404467c71a3eea761cf1776fae58d9939d2793
plzusername <MohamedFaris2210@gmail.com>
2024-02-07 16:35:21 +0300

    git gud

--- src/modules/Storage/itemStorage/addProject.js
+++ src/modules/Storage/itemStorage/addProject.js
@@ -2,26 +2,26 @@ import { viewAll } from '../../Logic/view-all-projects'
 import { projectGenerator } from '../../Logic/project-Generator'
 import { content } from '../../DOM/generate-content/generate-content-section'
 import { projectDomify } from '../../DOM/DOM-functions/generate-DOM-project'
+import { Storage, saveStorage } from '../storage-utils/save-storage'
 
 let generatedProject
 
 function addProjectToStorage(){
     if (typeof Storage == 'string'){
         let parsedStorage = JSON.parse(Storage)
-        parsedStorage.projects.push(generatedProject)
+        parsedStorage.projects = viewAll.projects
         return parsedStorage
     }
-    Storage.projects.push(generatedProject)
+    Storage.projects = viewAll.projects
     return Storage
-
 }
 
 function addProjet(){
-    const Storage=localStorage.getItem('Storage')
-    const generatedProject = new projectGenerator()
+    generatedProject = new projectGenerator()
 
-    Storage.projects.push(generatedProject)
     viewAll.addProject(generatedProject)
+
+    saveStorage(addProjectToStorage)
 
     content.appendChild(projectDomify(generatedProject))
 
