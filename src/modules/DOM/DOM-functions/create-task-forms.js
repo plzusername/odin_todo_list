4da64377197969cd5f3e720e50db5e7db8b892d5
plzusername <MohamedFaris2210@gmail.com>
2024-02-08 14:38:17 +0300

    Add preventdefault Function to prevent page reload

--- src/modules/DOM/DOM-functions/create-task-forms.js
+++ src/modules/DOM/DOM-functions/create-task-forms.js
@@ -24,25 +24,29 @@ function createGenerateTaskForm(){
     const formHeader=createElement({class:'task-form-header'}, 'h1', `task`, [])
 
     const titleInput=createElement({class:'title-input-task'}, 'input', '', [])
+    titleInput.required=true
     const titleInputSection=createElement({class:'title-input-task-section'}, 'label', 'Title:', [titleInput])
 
     const descriptionInput=createElement({class:'description-input-task'}, 'input', '', [])
+    descriptionInput.required=true
     const descriptionInputSection=createElement({class:'description-input-task-section'}, 'label', 'Description:', [descriptionInput])
 
-    const priorityInput=createElement({class:'priority-input-task'}, 'input', '', [])
+    const priorityInput=createElement({class:'priority-input-task' }, 'input', '', [])
+    priorityInput.required=true
     const priorityInputSection=createElement({class:'priority-input-task-section'}, 'label', 'Priority:', [priorityInput])
 
-    const dueDateInput=createElement({class:'due-date-input-task'}, 'input', '', [])
+    const dueDateInput=createElement({class:'due-date-input-task', type:'date'}, 'input', '', [])
+    dueDateInput.required=true
     const dueDateInputSection=createElement({class:'due-date-input-task-section'}, 'label', 'Due Date:', [dueDateInput])
 
-    const submitButton=createElement({class:'task-submit-button'} ,'button', `task`)
+    const submitButton=createElement({class:'task-submit-button', type:'submit'} ,'button', `task`)
 
 
-    const taskForm=createElement({class:`create-task-form`}, 'div', '', [formHeader,titleInputSection,descriptionInputSection,priorityInputSection,dueDateInputSection,submitButton])
+    const taskForm=createElement({class:`create-task-form`}, 'form', '', [formHeader,titleInputSection,descriptionInputSection,priorityInputSection,dueDateInputSection,submitButton])
     
     formHeader.textContent = 'Create form'
 
-    submitButton.addEventListener('click' , ()=>{
+    taskForm.addEventListener('submit',()=>{
         const parentProject = document.querySelector(`.project-container[data-id="${window.currentTaskForm}"]`)
 
         generatedTask = new taskGenerator(titleInput.value , descriptionInput.value , viewAll.projects[window.currentTaskForm], false ,  priorityInput.value , dueDateInput.value, )
@@ -55,9 +59,23 @@ function createGenerateTaskForm(){
 
         taskForm.classList.remove('visible')
 
-
-
+        event.preventDefault()
     })
+
+    submitButton.addEventListener('click' , ()=>{
+        // const parentProject = document.querySelector(`.project-container[data-id="${window.currentTaskForm}"]`)
+
+        // generatedTask = new taskGenerator(titleInput.value , descriptionInput.value , viewAll.projects[window.currentTaskForm], false ,  priorityInput.value , dueDateInput.value, )
+
+        // viewAll.addProject(generatedTask)
+
+        // saveStorage(addTaskToProjectStorage)
+
+        // parentProject.appendChild(createTask(generatedTask))
+
+        // taskForm.classList.remove('visible')
+    })
+
 
     return taskForm
 
