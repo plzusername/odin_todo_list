0f292b8ee76c2a17f60c0c292c92aa4f7ad05313
plzusername <MohamedFaris2210@gmail.com>
2024-02-09 14:21:59 +0300

    Add event listeners to remove task function

--- src/modules/Storage/EventListeners.js
+++ src/modules/Storage/EventListeners.js
@@ -1,5 +1,6 @@
 import {addProjet} from './itemStorage/addProject'
 import {showCreateTaskForm} from './itemStorage/showForm'
+import {removeTaskfromPage} from './itemStorage/removeTask'
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const addProjectButton = document.querySelector('.Add-project')
@@ -11,9 +12,13 @@ document.body.addEventListener('click',()=>{
 
     if (document.body.contains(document.querySelector('.Add-task'))){
         const addTaskButtons = document.querySelectorAll('.Add-task')
+        const removeTaskButtons = document.querySelectorAll('.task-delete-icon')
 
         addTaskButtons.forEach(addTaskButton => {
             addTaskButton.addEventListener('click',showCreateTaskForm)
+        })
+        removeTaskButtons.forEach(removeTaskButton =>{
+            removeTaskButton.addEventListener('click',removeTaskfromPage)
         })
     
     }
