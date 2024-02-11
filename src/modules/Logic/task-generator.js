38ab308344ec64e26d4b29f00e2b2570ba4d314a
plzusername <MohamedFaris2210@gmail.com>
2024-02-11 16:36:28 +0300

    Make task more independant from parentProject

--- src/modules/Logic/task-generator.js
+++ src/modules/Logic/task-generator.js
@@ -6,8 +6,6 @@ class taskGenerator{
         this.checked=checked
         this.priority=priority
         this.due_date=due_date
-        this.id = parent_project.tasks.length
-
     }
 
     taskEdit(newTitle, newDescription, newPriority, newDueDate){
@@ -15,13 +13,6 @@ class taskGenerator{
         this.description=newDescription
         this.priority=newPriority
         this.due_date=newDueDate
-    }
-
-    changeProject(newParentProject){
-        this.parent_project.removeTask(this.id,1)
-        this.parent_project=newParentProject
-        this.id=newParentProject.tasks.length
-        newParentProject.addTask(this.title,this.description,this.checked,this.priority,this.due_date)
     }
 
     check(){
