0c9d9e3188c5050d9e70fa1efb480a94fae99266
plzusername <MohamedFaris2210@gmail.com>
2024-02-13 13:16:11 +0300

    Redo element id's as well as data id's

--- src/modules/Logic/redoIds.js
+++ src/modules/Logic/redoIds.js
@@ -1,7 +1,15 @@
-function redoIds(items){
+import { content } from '../DOM/generate-content/generate-content-section'
+
+function redoIds(items, elements){
     items.forEach((item, index) => {
         item.id = index
     });
+
+    const elementsToLoop = document.querySelectorAll(elements)
+
+    elementsToLoop.forEach((element, index) =>{
+        element.dataset.id = index
+    })
 }
 
 export{ redoIds }
