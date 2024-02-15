84200e181fa4bb162eae0901bf7053742a32a9ba
plzusername <MohamedFaris2210@gmail.com>
2024-02-15 16:51:49 +0300

    Import storage functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import { header } from './modules/DOM/generate-content/generate-header.js'
 import { footer } from './modules/DOM/generate-content/generate-footer.js'
 import { sideBar } from './modules/DOM/generate-content/generate-sideBar.js';
-import { projectGenerator } from './modules/Logic/project-Generator.js';
-import { viewAll } from './modules/Logic/view-all-projects.js';
 import { content } from './modules/DOM/generate-content/generate-content-section.js';
 import '../src/modules/Styles/main.css'
 
@@ -13,3 +11,5 @@ document.body.appendChild(content)
 document.body.appendChild(footer)
 
 // renderSotrage()
+
+import './modules/Storage/EventListeners.js'
