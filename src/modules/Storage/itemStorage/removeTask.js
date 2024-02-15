9d944d7ad537d20794b783bf3954766fcc8dab32
plzusername <MohamedFaris2210@gmail.com>
2024-02-15 17:01:55 +0300

    Try to add Storage function to deleting tasks(fail)

--- src/modules/Storage/itemStorage/removeTask.js
+++ src/modules/Storage/itemStorage/removeTask.js
@@ -1,6 +1,6 @@
 import { viewAll } from '../../Logic/view-all-projects'
 import { Storage, saveStorage } from '../storage-utils/save-storage'
-import { filterEmptyProjects } from '../../Logic/remove-empty-project'
+import { redoIds } from '../../Logic/redoIds'
 
 const CircularJSON = require('circular-json')
 
@@ -8,40 +8,34 @@ let removedTask
 
 function addProjectToStorage(){
     if (typeof Storage == 'string'){
-        let parsedStorage = CircularJSON.parse(Storage)
-        parsedStorage.projects[event.target.closest('.project-container').dataset.id].tasks.splice(event.target.closest('.task-container').dataset.id,1)
-        Storage.projects = filterEmptyProjects(Storage.projects)
+        // let parsedStorage = CircularJSON.parse(Storage)
+        // parsedStorage.projects[event.target.closest('.project-container').dataset.id].removeTask(removedTask)
+        // redoIds(parsedStorage.projects[event.target.closest('.project-container').dataset.id].tasks)
         return parsedStorage
     }
-    Storage.projects[event.target.closest('.project-container').dataset.id].tasks.splice(event.target.closest('.task-container').dataset.id,1)
-    Storage.projects = filterEmptyProjects(Storage.projects)
+    // console.log(Storage.projects[event.target.closest('.project-container').dataset.id])
+    // Storage.projects[event.target.closest('.project-container').dataset.id].removeTask(removedTask)
+    // redoIds(Storage.projects[event.target.closest('.project-container').dataset.id].tasks)
     return Storage
 }
 
 function removeTaskfromPage(){
 
     const removeButton = event.target
-    const parentProjectObj = viewAll.projects[removeButton.closest('.project-container').dataset.id]
+    const parentProjectElement = removeButton.closest('.project-container')
+    const parentProjectObj = viewAll.projects[parentProjectElement.dataset.id]
 
-    removedTask = parentProjectObj[removeButton.dataset.id]
+    removedTask = parentProjectObj.tasks[removeButton.closest('.task-container').dataset.id]
 
     parentProjectObj.removeTask(removedTask)
 
-    viewAll.projects = filterEmptyProjects(viewAll.projects)
-
     saveStorage(addProjectToStorage)
 
-
-    if(removeButton.closest('.project-container').children.length == 3){
-        const correspndingProjectFilter = document.querySelector(`.project-filter[data-id="${removeButton.closest('.project-container').dataset.id}"]`)
-
-        removeButton.closest('.project-container').remove()
-        correspndingProjectFilter.remove()
-
-        return null
-    }
-
     removeButton.closest('.task-container').remove()
+
+    const parent_projects_tasks = parentProjectElement.querySelectorAll('.task-container')
+
+    redoIds(parentProjectObj.tasks, parent_projects_tasks)
 
 }
 
