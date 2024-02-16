babda98cb98b117be543912508aeb3127a89798c
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 21:11:38 +0300

    Clean code,populate form inputs with task values

---
+++ src/modules/Storage/itemStorage/showEditTaskForm.js
@@ -0,0 +1,28 @@
+function showEditTaskForm(){
+    const taskFormElement = document.querySelector('.edit-task-form')
+
+    taskFormElement.classList.add('visible')
+
+    window.taskToBeEditedIndex = event.target.closest('.task-container').dataset.id
+    window.currentTaskForm = event.target.closest('.project-container').dataset.id
+
+    const titleInput = taskFormElement.querySelector('.title-input-task-edit')
+    const descriptionInput = taskFormElement.querySelector('.description-input-task-edit')
+    const priorityInput = taskFormElement.querySelector('.priority-input-task-edit')
+    const dateInput = taskFormElement.querySelector('.due-date-input-task-edit')
+
+    const taskToBeEdited = event.target.closest('.task-container')
+
+    const taskTitle = taskToBeEdited.querySelector('.task-title').innerText
+    const taskDescription = taskToBeEdited.querySelector('.task-description').innerText
+    const taskPriority = taskToBeEdited.dataset.priority
+    const taskDate = taskToBeEdited.querySelector('.task-right-side').querySelector('.task-date-input').value
+
+    titleInput.value = taskTitle
+    descriptionInput.value = taskDescription
+    priorityInput.value = taskPriority
+    dateInput.value = taskDate
+}
+
+export {showEditTaskForm}
+
