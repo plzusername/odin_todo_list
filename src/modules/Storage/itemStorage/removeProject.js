9c1a089ca62b425c2ee9c954ed2b11559c89c7bd
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 15:52:43 +0300

    Add function remove deleted projects from Storage

---
+++ src/modules/Storage/itemStorage/removeProject.js
@@ -0,0 +1,41 @@
+import { viewAll } from '../../Logic/view-all-projects'
+import { Storage, saveStorage } from '../storage-utils/save-storage'
+import { redoIds } from '../../Logic/redoIds'
+
+let removedProject
+
+function addProjectToStorage(){
+    Storage.projects.splice(removedProject.id, 1)
+    redoIds(Storage.projects)
+    return Storage
+}
+
+function removeProjectFromPage(){
+
+    const removeButton = event.target
+
+    const removedProjectDOM = removeButton.closest('.project-container')
+
+    const correspondinngFilter = document.querySelector(`.filter-container[data-id="${removedProjectDOM.dataset.id}"] `)
+
+    removedProject = viewAll.projects[removedProjectDOM.dataset.id]
+
+    viewAll.removeProject(removedProject)
+
+    saveStorage(addProjectToStorage)
+
+    removedProjectDOM.remove()
+
+    const parent_projects_tasks = document.querySelectorAll('.project-container')
+
+    redoIds(viewAll.projects, parent_projects_tasks)
+
+    correspondinngFilter.remove()
+
+    const allFilters = document.querySelectorAll('.project-filter')
+
+    redoIds('none', allFilters)
+
+}
+
+export {removeProjectFromPage}
