424432100436a946dfed37688faba39ea9c5772e
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 17:45:37 +0300

    Clean up function

--- src/modules/Storage/itemStorage/checkTask.js
+++ src/modules/Storage/itemStorage/checkTask.js
@@ -1,9 +1,5 @@
 import { viewAll } from '../../Logic/view-all-projects'
-import { projectGenerator } from '../../Logic/project-Generator'
-import { content } from '../../DOM/generate-content/generate-content-section'
-import { projectDomify } from '../../DOM/DOM-functions/generate-DOM-project'
 import { Storage, saveStorage } from '../storage-utils/save-storage'
-import { generateFilter } from '../../DOM/DOM-functions/generate-filter'
 
 function addProjectToStorage(){
     return Storage
@@ -11,7 +7,6 @@ function addProjectToStorage(){
 
 function checkTask(){
     const checkBox = event.target
-    console.log('HI THERE')
 
     const parentTask = checkBox.closest('.task-container')
     parentTask.classList.toggle('checked')
