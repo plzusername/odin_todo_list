983717ef68f12ed9181c6aa6aa49e09468c657c5
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 15:55:17 +0300

    Make data input opyional alongside element input

--- src/modules/Logic/redoIds.js
+++ src/modules/Logic/redoIds.js
@@ -1,8 +1,9 @@
 function redoIds(items, elements = undefined){
-    items.forEach((item, index) => {
-        item.id = index
-    });
-
+    if(items != 'none'){
+        items.forEach((item, index) => {
+            item.id = index
+        });
+    }
     if(elements){
         elements.forEach((element, index) =>{
             element.dataset.id = index
