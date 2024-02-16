225c5e57ebc5b59a814aef961941d9dfa9822f90
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 15:50:22 +0300

    Add event listeners for removing projects

--- src/modules/Storage/EventListeners.js
+++ src/modules/Storage/EventListeners.js
@@ -1,6 +1,7 @@
 import {addProjet} from './itemStorage/addProject'
 import {showCreateTaskForm} from './itemStorage/showForm'
 import {removeTaskfromPage} from './itemStorage/removeTask'
+import { removeProjectFromPage } from './itemStorage/removeProject'
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const addProjectButton = document.querySelector('.Add-project')
@@ -13,12 +14,16 @@ document.body.addEventListener('click',()=>{
     if (document.body.contains(document.querySelector('.Add-task'))){
         const addTaskButtons = document.querySelectorAll('.Add-task')
         const removeTaskButtons = document.querySelectorAll('.task-delete-icon')
+        const removeProjectButtons = document.querySelectorAll('.delete-project-icon')
 
         addTaskButtons.forEach(addTaskButton => {
             addTaskButton.addEventListener('click',showCreateTaskForm)
         })
         removeTaskButtons.forEach(removeTaskButton =>{
             removeTaskButton.addEventListener('click',removeTaskfromPage)
+        })
+        removeProjectButtons.forEach(removeProjectButton =>{
+            removeProjectButton.addEventListener('click',removeProjectFromPage)
         })
     
     }
