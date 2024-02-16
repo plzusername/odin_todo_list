624d79157ed40229370eaf73237c98189391b4b8
plzusername <MohamedFaris2210@gmail.com>
2024-02-16 21:11:23 +0300

    Remove redundant code

--- src/modules/Storage/itemStorage/showForm.js
+++ src/modules/Storage/itemStorage/showForm.js
@@ -1,39 +1,9 @@
-import { viewAll } from '../../Logic/view-all-projects'
-import { taskGenerator } from '../../Logic/task-generator'
-import { content, taskForm } from '../../DOM/generate-content/generate-content-section'
-import { projectDomify } from '../../DOM/DOM-functions/generate-DOM-project'
-import { Storage, saveStorage } from '../storage-utils/save-storage'
-
-let generatedTask
-
-const CircularJSON = require('circular-json')
-
-
-function addProjectToStorage(){
-    if (typeof Storage == 'string'){
-        let parsedStorage = CircularJSON.parse(Storage)
-        parsedStorage.projects.push(generatedProject)
-        return parsedStorage
-    }
-    Storage.projects.push(generatedProject)
-    return Storage
-}
-
 function showCreateTaskForm(){
     const taskFormElement = document.querySelector('.create-task-form')
 
     taskFormElement.classList.add('visible')
 
     window.currentTaskForm = event.target.closest('.project-container').dataset.id
-    
-    // generatedProject = new taskGenerator()
-
-    // viewAll.addProject(generatedProject)
-
-    // saveStorage(addProjectToStorage)
-
-    // content.appendChild(projectDomify(generatedProject))
-
 }
 
 export {showCreateTaskForm}
