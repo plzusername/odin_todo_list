62251a61d4c4ae2a58689e64ca5ea4b6fc2dd9ff
plzusername <MohamedFaris2210@gmail.com>
2024-02-28 18:21:26 +0300

    Add date filtering form to content section

--- src/modules/DOM/generate-content/generate-content-section.js
+++ src/modules/DOM/generate-content/generate-content-section.js
@@ -1,11 +1,12 @@
 import { createElement } from '../utility/createElement.js'
 import { createEditTaskForm, createGenerateTaskForm } from '../DOM-functions/create-task-forms.js'
 import { createEditNoteForm, createGenerateNoteForm } from '../DOM-functions/create-note-forms.js'
+import { createNumberInputForm } from '../DOM-functions/createNumberInputForm.js'
 
 function createContentSection(){
     
 
-    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm()])
+    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm(), createNumberInputForm()])
 
     return content_section
 }
