e0388c6ac5599acecdad850ba8e41915c4e4431b
plzusername <MohamedFaris2210@gmail.com>
2024-03-01 14:38:27 +0300

    Add function for domifying notes

---
+++ src/modules/DOM/DOM-functions/noteDomification.js
@@ -0,0 +1,27 @@
+import { createElement } from "../utility/createElement";
+
+function noteDomification(note){
+    const titleText=document.createTextNode(note.title)
+    const descriptionText=document.createTextNode(note.description)
+
+    const noteTitle=createElement({class:'note-title'}, 'p', '', [titleText])
+    const noteDescription=createElement({class:'note-description'}, 'p',  '', [descriptionText])
+    const noteDelete=createElement({class:'note-delete-icon fa-solid fa-trash-can'}, 'i',  '', [])
+    const noteEdit=createElement({class:'note-edit-icon fa-regular fa-pen-to-square'}, 'i',  '', [])
+
+    const noteText=createElement({class:'note-text-container'}, 'div', '', [noteTitle, noteDescription])
+
+    const leftSide=createElement({class:'note-left-side'}, 'div', '', [noteText])
+    const rightSide=createElement({class:'note-right-side'}, 'div', '', [noteDelete, noteEdit])
+
+    const noteContainer=createElement({class:'note-container'}, 'div', '', [leftSide,rightSide])
+
+    noteContainer.dataset.id = note.id
+    noteEdit.dataset.id = note.id
+    noteDelete.dataset.id = note.id
+
+    return noteContainer
+
+}
+
+export {noteDomification}
