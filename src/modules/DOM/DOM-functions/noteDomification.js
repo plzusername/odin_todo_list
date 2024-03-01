8ca1fa9b77fb2458537968a3e727e0ffbdf0d7dd
plzusername <MohamedFaris2210@gmail.com>
2024-03-01 17:28:56 +0300

    Remove redundant text-container

--- src/modules/DOM/DOM-functions/noteDomification.js
+++ src/modules/DOM/DOM-functions/noteDomification.js
@@ -4,17 +4,16 @@ function noteDomification(note){
     const titleText=document.createTextNode(note.title)
     const descriptionText=document.createTextNode(note.description)
 
-    const noteTitle=createElement({class:'note-title'}, 'p', '', [titleText])
+    const noteTitle=createElement({class:'note-title'}, 'h1', '', [titleText])
     const noteDescription=createElement({class:'note-description'}, 'p',  '', [descriptionText])
     const noteDelete=createElement({class:'note-delete-icon fa-solid fa-trash-can'}, 'i',  '', [])
     const noteEdit=createElement({class:'note-edit-icon fa-regular fa-pen-to-square'}, 'i',  '', [])
 
     const noteText=createElement({class:'note-text-container'}, 'div', '', [noteTitle, noteDescription])
 
-    const leftSide=createElement({class:'note-left-side'}, 'div', '', [noteText])
     const rightSide=createElement({class:'note-right-side'}, 'div', '', [noteDelete, noteEdit])
 
-    const noteContainer=createElement({class:'note-container'}, 'div', '', [leftSide,rightSide])
+    const noteContainer=createElement({class:'note-container'}, 'div', '', [noteText,rightSide])
 
     noteContainer.dataset.id = note.id
     noteEdit.dataset.id = note.id
