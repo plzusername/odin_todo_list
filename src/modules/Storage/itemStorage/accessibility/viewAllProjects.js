6ee6dc48333519169652261c4790543f7455ceb3
plzusername <MohamedFaris2210@gmail.com>
2024-03-01 17:33:21 +0300

    Use sqitchBeweenSections Function

--- src/modules/Storage/itemStorage/accessibility/viewAllProjects.js
+++ src/modules/Storage/itemStorage/accessibility/viewAllProjects.js
@@ -1,17 +1,12 @@
 import { content } from "../../../DOM/generate-content/generate-content-section";
+import { switchBetweenSections } from "./sectionSwitching";
 
 function viewProjects(){
-    const projects = content.querySelectorAll('.project-container')
 
-    projects.forEach(project =>{
-        project.remove()
-    })
+    switchBetweenSections()
 
     window.projects.forEach(project => {
         content.appendChild(project)
-        if(project.style.display == 'none'){
-            project.style.display = 'block'
-        }
     });
 }
 
