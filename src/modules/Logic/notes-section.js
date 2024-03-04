211baab4d230d491b8fcd922614d6111b809b6d1
plzusername <MohamedFaris2210@gmail.com>
2024-03-04 18:44:12 +0300

    Add note to list, dont make new note

--- src/modules/Logic/notes-section.js
+++ src/modules/Logic/notes-section.js
@@ -4,14 +4,15 @@ import { notesGenerator } from "./notes-generator"
 class notesSection{
     static notes=[]
 
-    static addItem(title, description){
-        notesSection.notes.push(new notesGenerator(title, description))
+    static addItem(note){
+        notesSection.notes.push()
     }
-
+    
     static removeNote(note){
         notesSection.notes.splice(note.id,1)
         trashSection.addTrashedTask(note)
     }
+
 }
 
 export {notesSection}
