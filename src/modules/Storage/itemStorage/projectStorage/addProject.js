7aca3796a50ba8d1e387af9d01cff6064004d49f
plzusername <MohamedFaris2210@gmail.com>
2024-03-06 18:02:08 +0300

    Remove redundant code

--- src/modules/Storage/itemStorage/projectStorage/addProject.js
+++ src/modules/Storage/itemStorage/projectStorage/addProject.js
@@ -34,13 +34,7 @@ function addProjet(){
     const sideBarProjects = document.querySelector('.sideBar-projects-section')
     sideBarProjects.appendChild(projectFilter)
 
-    window.projects.forEach(project => {
-        content.appendChild(project)
-    });    
-
     content.appendChild(DOMproject)
-
-    window.projects = Array.from(document.querySelector('.content-section').querySelectorAll('.project-container')) 
 
     viewProjects()
 
--- src/modules/Storage/itemStorage/projectStorage/removeProject.js
+++ src/modules/Storage/itemStorage/projectStorage/removeProject.js
@@ -2,7 +2,6 @@ import { viewAll } from '../../../Logic/view-all-projects'
 import { Storage, saveStorage } from '../../storage-utils/save-storage'
 import { redoIds } from '../../../Logic/redoIds'
 import { viewProjects } from '../accessibility/viewAllProjects'
-import { content } from '../../../DOM/generate-content/generate-content-section'
 
 let removedProject
 
@@ -13,39 +12,25 @@ function addProjectToStorage(){
 }
 
 function removeProjectFromPage(){
-
     const removeButton = event.target
-
     const removedProjectDOM = removeButton.closest('.project-container')
-
     const correspondinngFilter = document.querySelector(`.filter-container[data-id="${removedProjectDOM.dataset.id}"] `)
 
     removedProject = viewAll.projects[removedProjectDOM.dataset.id]
 
     viewAll.removeProject(removedProject)
-
     saveStorage(addProjectToStorage)
-
-    window.projects.forEach(project => {
-        content.appendChild(project)
-    });
 
     removedProjectDOM.remove()
 
-    window.projects = Array.from(document.querySelector('.content-section').querySelectorAll('.project-container')) 
-    
-    // viewProjects()
-
-
     const parent_projects_tasks = document.querySelectorAll('.project-container')
-
     redoIds(viewAll.projects, parent_projects_tasks)
 
     correspondinngFilter.remove()
-
     const allFilters = document.querySelectorAll('.project-filter')
-
     redoIds('none', allFilters)
+
+    viewProjects()
 
 
 }
