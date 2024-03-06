0226c226ec66f7329caffaab2b7c05338144ae55
plzusername <MohamedFaris2210@gmail.com>
2024-03-06 17:32:13 +0300

    Populate diffrent arrays on page load

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { footer } from './modules/DOM/generate-content/generate-footer.js'
 import { sideBar } from './modules/DOM/generate-content/generate-sideBar.js';
 import { content } from './modules/DOM/generate-content/generate-content-section.js';
 import '../src/modules/Styles/main.css'
+import { populateArrays } from './modules/Storage/storage-utils/populateArrays.js';
 window.projects = []
-
 
 document.body.appendChild(header)
 document.body.appendChild(sideBar)
 document.body.appendChild(content)
 document.body.appendChild(footer)
 
+populateArrays()
 // renderSotrage()
 
 import './modules/Storage/EventListeners.js'
