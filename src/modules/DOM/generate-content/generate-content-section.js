3b36047b043ca1adf7d54e2b800e075b799e0e0d
plzusername <MohamedFaris2210@gmail.com>
2024-03-06 19:14:03 +0300

    Add trash section to content section

--- src/modules/DOM/generate-content/generate-content-section.js
+++ src/modules/DOM/generate-content/generate-content-section.js
@@ -2,11 +2,10 @@ import { createElement } from '../utility/createElement.js'
 import { createEditTaskForm, createGenerateTaskForm } from '../DOM-functions/create-task-forms.js'
 import { createEditNoteForm, createGenerateNoteForm } from '../DOM-functions/create-note-forms.js'
 import { createNumberInputForm } from '../DOM-functions/createNumberInputForm.js'
+import { createTrashSection } from '../DOM-functions/createTrashSection.js'
 
 function createContentSection(){
-    
-
-    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm(), createNumberInputForm()])
+    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm(), createNumberInputForm(), createTrashSection()])
 
     return content_section
 }
