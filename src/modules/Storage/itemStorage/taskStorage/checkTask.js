39adff248b1d24f0ed490aea67cb58feb1b67416
plzusername <MohamedFaris2210@gmail.com>
2024-03-08 16:22:34 +0300

    Fix storage, make dom changer function

--- src/modules/Storage/itemStorage/taskStorage/checkTask.js
+++ src/modules/Storage/itemStorage/taskStorage/checkTask.js
@@ -2,22 +2,64 @@ import { viewAll } from '../../../Logic/view-all-projects'
 import { Storage, saveStorage } from '../../storage-utils/save-storage'
 
 function addProjectToStorage(){
+    Storage.projects = viewAll.projects
     return Storage
+}
+
+function checkDOMchanges(task,taskDOM){
+    taskDOM.querySelector('input[type="checkbox"]').checked = task.checked
+    if(task.checked){
+        taskDOM.classList.add('checked')
+        return null
+    }
+    taskDOM.classList.remove('checked')
 }
 
 function checkTask(){
     const checkBox = event.target
 
     const parentTask = checkBox.closest('.task-container')
-    parentTask.classList.toggle('checked')
 
     const parentTaskObject = viewAll.projects[checkBox.closest('.project-container').dataset.id].tasks[parentTask.dataset.id]
     parentTaskObject.check()
 
-    saveStorage(addProjectToStorage)
+    checkDOMchanges(parentTaskObject, parentTask)
 
-    window.projects = Array.from(document.querySelector('.content-section').querySelectorAll('.project-container')) 
+    saveStorage(addProjectToStorage)
 
 }
 
-export {checkTask}
+export {checkTask, checkDOMchanges}
+
+
+// import { viewAll } from '../../../Logic/view-all-projects'
+// import { Storage, saveStorage } from '../../storage-utils/save-storage'
+// import { regeneratePrototype } from '../../../Logic/regeneratePrototype'
+
+// let checkBox
+// let parentTask
+// let parentTaskObject
+
+// function addProjectToStorage(){
+//     let regeneratedStorage = regeneratePrototype()
+//     console.log(regeneratedStorage.projects[checkBox.closest('.project-container').dataset.id].tasks[checkBox.closest('.project-container').dataset.id])
+//     regeneratedStorage.projects[checkBox.closest('.project-container').dataset.id].tasks[checkBox.closest('.project-container').dataset.id].check()
+//     return regeneratedStorage
+// }
+
+// function checkTask(){
+//     checkBox = event.target
+
+//     parentTask = checkBox.closest('.task-container')
+//     parentTask.classList.toggle('checked')
+
+//     console.log(viewAll.projects[checkBox.closest('.project-container').dataset.id])
+
+//     parentTaskObject = viewAll.projects[checkBox.closest('.project-container').dataset.id].tasks[parentTask.dataset.id]
+//     parentTaskObject.check()
+
+//     saveStorage(addProjectToStorage)
+
+// }
+
+// export {checkTask}
