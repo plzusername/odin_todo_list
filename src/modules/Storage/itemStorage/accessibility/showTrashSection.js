8980c8187a07898999dbab380abbc9d6253d3736
plzusername <MohamedFaris2210@gmail.com>
2024-03-12 21:44:56 +0300

    Add function to show trash section

---
+++ src/modules/Storage/itemStorage/accessibility/showTrashSection.js
@@ -0,0 +1,9 @@
+import { showEffect } from "../../../DOM/generate-content/generate-content-section"
+
+function showTrashSection(){
+    const trashSection = document.querySelector('.trash-section')
+
+    showEffect(trashSection)
+}
+
+export {showTrashSection}
