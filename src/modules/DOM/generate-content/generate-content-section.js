83725b0459fc10ba8bc0a9552957b159d0a92a70
plzusername <MohamedFaris2210@gmail.com>
2024-03-12 22:02:00 +0300

    Add hideAffect and showAffect functions

--- src/modules/DOM/generate-content/generate-content-section.js
+++ src/modules/DOM/generate-content/generate-content-section.js
@@ -7,11 +7,32 @@ import { createTrashSection } from '../DOM-functions/createTrashSection.js'
 const formAffect = createElement({class:'form-affect'}, 'div', '', [])
 
 function createContentSection(){
-    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm(), createNumberInputForm(), ])
+    const content_section=createElement({class:'content-section'}, 'div', '', [createEditNoteForm(), createEditTaskForm(), createGenerateNoteForm(), createGenerateTaskForm(), createNumberInputForm(), createTrashSection()])
 
     return content_section
 }
 
+function showEffect(formToShow){
+    const formAffect = document.querySelector('.form-affect')
+    formAffect.classList.add('form-affect-visible')
+    formToShow.classList.add('visible')
+}
+
+function hideEffect(formToHide){
+    const formAffect = document.querySelector('.form-affect')
+    const delayTime = 300
+    formAffect.classList.remove('form-affect-visible')
+    formToHide.classList.remove('visible')
+    setTimeout(()=>{
+        if(formToHide.querySelectorAll('input').length > 0){
+            formToHide.querySelectorAll('input').forEach(input =>{
+                input.value = ''
+        })
+        if(formToHide.querySelector('select') != null) formToHide.querySelector('select').selectedIndex = 0    
+    }    
+    },delayTime)
+}
+
 let content = createContentSection()
 
-export {content, formAffect}
+export {content, formAffect, showEffect, hideEffect}
