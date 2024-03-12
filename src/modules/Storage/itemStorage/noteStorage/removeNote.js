0ceede2e4f9b210027e79adc7c5bd28a110cf4e0
plzusername <MohamedFaris2210@gmail.com>
2024-03-12 21:41:57 +0300

    Add to trash section after deleting an item

--- src/modules/Storage/itemStorage/noteStorage/removeNote.js
+++ src/modules/Storage/itemStorage/noteStorage/removeNote.js
@@ -1,24 +1,40 @@
 import { notesSection } from '../../../Logic/notes-section' 
 import { saveStorage, Storage } from '../../storage-utils/save-storage'
 import { redoIds } from '../../../Logic/redoIds'
+import { trashSection } from '../../../Logic/trash-section'
+import { createElement } from '../../../DOM/utility/createElement'
 
 let removedNote
 
 function addProjectToStorage(){
     Storage.notes = notesSection.notes
+    Storage.trash = trashSection.trashedItems
     return Storage
 }
 
 function removeNote(){
-
+    const DOMtrashSection = document.querySelector('.trash-section > .trash-items-container')
+    const restoreFromTrashIcon = createElement({class:'restore-from-trash-icon fa-solid fa-trash-can-arrow-up'}, 'i', '', [])
+    const removeFromTrashIcon = createElement({class:'remove-from-trash-icon fa-regular fa-circle-xmark'}, 'i', '', [])
+    const trashOptions = createElement({class:'trash-icons'}, 'div', '', [restoreFromTrashIcon, removeFromTrashIcon])
     const removeButton = event.target
-    removedNote = notesSection.notes[removeButton.closest('.note-container').dataset.id]
+    const DOMremovedNote = removeButton.closest('.note-container')
 
-    removeButton.closest('.note-container').remove()
+    const clonedNote = DOMremovedNote.cloneNode(true)
+    clonedNote.replaceChild(trashOptions, clonedNote.children[1])
 
-    const DOM_notes = document.querySelectorAll('.note-container')
+    removedNote = notesSection.notes[DOMremovedNote.dataset.id]
 
     notesSection.removeNote(removedNote)
+
+    DOMtrashSection.appendChild(clonedNote)
+
+    redoIds(trashSection.trashedItems, document.querySelectorAll('.trash-items-container > *'))
+
+    DOMremovedNote.remove()
+
+    const DOM_notes = document.querySelectorAll('.content-section > .note-container')
+    console.log(DOM_notes)
 
     redoIds(notesSection.notes, DOM_notes)
 
--- src/modules/Storage/itemStorage/taskStorage/removeTask.js
+++ src/modules/Storage/itemStorage/taskStorage/removeTask.js
@@ -1,17 +1,32 @@
+import { createElement } from '../../../DOM/utility/createElement'
 import { viewAll } from '../../../Logic/view-all-projects'
 import { Storage, saveStorage } from '../../storage-utils/save-storage'
 import { redoIds } from '../../../Logic/redoIds'
+import { trashSection } from '../../../Logic/trash-section'
 
 let removedTask
 
 function addProjectToStorage(){
     Storage.projects = viewAll.projects
+    Storage.trash = trashSection.trashedItems
     return Storage
 }
 
 function removeTaskfromPage(){
-
     const removeButton = event.target
+
+    const DOMtrashSection = document.querySelector('.trash-section > .trash-items-container')
+    const restoreFromTrashIcon = createElement({class:'restore-from-trash-icon fa-solid fa-trash-can-arrow-up'}, 'i', '', [])
+    const removeFromTrashIcon = createElement({class:'remove-from-trash-icon fa-regular fa-circle-xmark'}, 'i', '', [])
+    const trashOptions = createElement({class:'trash-icons'}, 'div', '', [restoreFromTrashIcon, removeFromTrashIcon])
+    const DOMremovedTask = removeButton.closest('.task-container')
+
+    const clonedTask = DOMremovedTask.cloneNode(true)
+    clonedTask.querySelector('.task-delete-icon').remove()
+    clonedTask.querySelector('.task-edit-icon').remove()
+    clonedTask.children[1].appendChild(trashOptions)
+
+
     const parentProjectElement = removeButton.closest('.project-container')
     const parentProjectObj = viewAll.projects[parentProjectElement.dataset.id]
 
@@ -19,13 +34,17 @@ function removeTaskfromPage(){
 
     parentProjectObj.removeTask(removedTask)
 
-    saveStorage(addProjectToStorage)
+    DOMtrashSection.appendChild(clonedTask)
 
-    removeButton.closest('.task-container').remove()
+    redoIds(trashSection.trashedItems, document.querySelectorAll('.trash-items-container > *'))
 
-    const parent_projects_tasks = parentProjectElement.querySelectorAll('.task-container')
+    DOMremovedTask.remove()
+
+    const parent_projects_tasks = parentProjectElement.querySelectorAll('.content-section > .project-container  .task-container')
 
     redoIds(parentProjectObj.tasks, parent_projects_tasks)
+
+    saveStorage(addProjectToStorage)
 
 }
 
