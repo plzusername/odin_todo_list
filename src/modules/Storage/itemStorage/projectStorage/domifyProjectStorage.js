8f7dee248d42ffa7a05331e641dc58e151356658
plzusername <MohamedFaris2210@gmail.com>
2024-03-13 15:23:03 +0300

    On render trashedItems call repurposeIcon function

--- src/modules/Storage/itemStorage/projectStorage/domifyProjectStorage.js
+++ src/modules/Storage/itemStorage/projectStorage/domifyProjectStorage.js
@@ -2,6 +2,9 @@ import { content } from "../../../DOM/generate-content/generate-content-section"
 import { Storage } from "../../storage-utils/save-storage"
 import { projectDomify } from "../../../DOM/DOM-functions/generate-DOM-project";
 import { generateFilter } from "../../../DOM/DOM-functions/generate-filter";
+import { createTask } from "../../../DOM/DOM-functions/create-task-DOM";
+import { noteDomification } from "../../../DOM/DOM-functions/noteDomification";
+import { repurposeIcons } from "../../../DOM/DOM-functions/createTrashSection";
 
 function domifyStorageProjects(){
     const DOMprojects = []
@@ -21,7 +24,19 @@ function reinstateFilters(){
         projectFilter.dataset.id = project.id
         sideBarProjects.appendChild(projectFilter)
     });
+}
+function domifyStorageTrash(){
+    const DOMtrash = []
+    const trashItemsSection = document.querySelector('.trash-items-container')
 
+    Storage.trash.forEach(trashedItem => {
+        const DOMitem = trashedItem.priority == undefined ? noteDomification(trashedItem) : createTask(trashedItem)
+        const trashItem = repurposeIcons(DOMitem)
+        trashItemsSection.appendChild(trashItem)
+        DOMtrash.push(DOMitem)
+    });
+
+    return DOMtrash
 }
 
-export {domifyStorageProjects, reinstateFilters}
+export {domifyStorageProjects, reinstateFilters, domifyStorageTrash}
