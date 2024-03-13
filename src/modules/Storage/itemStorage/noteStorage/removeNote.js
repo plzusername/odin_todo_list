4ef2f4da5aa01362c680891a1fa261ab4f793de9
plzusername <MohamedFaris2210@gmail.com>
2024-03-13 15:18:13 +0300

    Use icons function to make item icons trash icons

--- src/modules/Storage/itemStorage/noteStorage/removeNote.js
+++ src/modules/Storage/itemStorage/noteStorage/removeNote.js
@@ -2,7 +2,7 @@ import { notesSection } from '../../../Logic/notes-section'
 import { saveStorage, Storage } from '../../storage-utils/save-storage'
 import { redoIds } from '../../../Logic/redoIds'
 import { trashSection } from '../../../Logic/trash-section'
-import { createElement } from '../../../DOM/utility/createElement'
+import { repurposeIcons } from '../../../DOM/DOM-functions/createTrashSection' 
 
 let removedNote
 
@@ -14,20 +14,18 @@ function addProjectToStorage(){
 
 function removeNote(){
     const DOMtrashSection = document.querySelector('.trash-section > .trash-items-container')
-    const restoreFromTrashIcon = createElement({class:'restore-from-trash-icon fa-solid fa-trash-can-arrow-up'}, 'i', '', [])
-    const removeFromTrashIcon = createElement({class:'remove-from-trash-icon fa-regular fa-circle-xmark'}, 'i', '', [])
-    const trashOptions = createElement({class:'trash-icons'}, 'div', '', [restoreFromTrashIcon, removeFromTrashIcon])
     const removeButton = event.target
     const DOMremovedNote = removeButton.closest('.note-container')
 
     const clonedNote = DOMremovedNote.cloneNode(true)
-    clonedNote.replaceChild(trashOptions, clonedNote.children[1])
+
+    const trashedNote = repurposeIcons(clonedNote)
 
     removedNote = notesSection.notes[DOMremovedNote.dataset.id]
 
     notesSection.removeNote(removedNote)
 
-    DOMtrashSection.appendChild(clonedNote)
+    DOMtrashSection.appendChild(trashedNote)
 
     redoIds(trashSection.trashedItems, document.querySelectorAll('.trash-items-container > *'))
 
--- src/modules/Storage/itemStorage/taskStorage/removeTask.js
+++ src/modules/Storage/itemStorage/taskStorage/removeTask.js
@@ -1,4 +1,4 @@
-import { createElement } from '../../../DOM/utility/createElement'
+import { repurposeIcons } from '../../../DOM/DOM-functions/createTrashSection'
 import { viewAll } from '../../../Logic/view-all-projects'
 import { Storage, saveStorage } from '../../storage-utils/save-storage'
 import { redoIds } from '../../../Logic/redoIds'
@@ -16,16 +16,11 @@ function removeTaskfromPage(){
     const removeButton = event.target
 
     const DOMtrashSection = document.querySelector('.trash-section > .trash-items-container')
-    const restoreFromTrashIcon = createElement({class:'restore-from-trash-icon fa-solid fa-trash-can-arrow-up'}, 'i', '', [])
-    const removeFromTrashIcon = createElement({class:'remove-from-trash-icon fa-regular fa-circle-xmark'}, 'i', '', [])
-    const trashOptions = createElement({class:'trash-icons'}, 'div', '', [restoreFromTrashIcon, removeFromTrashIcon])
     const DOMremovedTask = removeButton.closest('.task-container')
 
     const clonedTask = DOMremovedTask.cloneNode(true)
-    clonedTask.querySelector('.task-delete-icon').remove()
-    clonedTask.querySelector('.task-edit-icon').remove()
-    clonedTask.children[1].appendChild(trashOptions)
 
+    const trashedTask = repurposeIcons(clonedTask)
 
     const parentProjectElement = removeButton.closest('.project-container')
     const parentProjectObj = viewAll.projects[parentProjectElement.dataset.id]
@@ -34,7 +29,7 @@ function removeTaskfromPage(){
 
     parentProjectObj.removeTask(removedTask)
 
-    DOMtrashSection.appendChild(clonedTask)
+    DOMtrashSection.appendChild(trashedTask)
 
     redoIds(trashSection.trashedItems, document.querySelectorAll('.trash-items-container > *'))
 
