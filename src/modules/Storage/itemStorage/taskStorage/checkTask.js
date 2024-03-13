247cd2314b37362977eb8510c740c13b24f41eb4
plzusername <MohamedFaris2210@gmail.com>
2024-03-13 15:19:20 +0300

    Remove huge comment serving no purpose

--- src/modules/Storage/itemStorage/taskStorage/checkTask.js
+++ src/modules/Storage/itemStorage/taskStorage/checkTask.js
@@ -30,36 +30,3 @@ function checkTask(){
 }
 
 export {checkTask, checkDOMchanges}
-
-
-// import { viewAll } from '../../../Logic/view-all-projects'
-// import { Storage, saveStorage } from '../../storage-utils/save-storage'
-// import { regeneratePrototype } from '../../../Logic/regeneratePrototype'
-
-// let checkBox
-// let parentTask
-// let parentTaskObject
-
-// function addProjectToStorage(){
-//     let regeneratedStorage = regeneratePrototype()
-//     console.log(regeneratedStorage.projects[checkBox.closest('.project-container').dataset.id].tasks[checkBox.closest('.project-container').dataset.id])
-//     regeneratedStorage.projects[checkBox.closest('.project-container').dataset.id].tasks[checkBox.closest('.project-container').dataset.id].check()
-//     return regeneratedStorage
-// }
-
-// function checkTask(){
-//     checkBox = event.target
-
-//     parentTask = checkBox.closest('.task-container')
-//     parentTask.classList.toggle('checked')
-
-//     console.log(viewAll.projects[checkBox.closest('.project-container').dataset.id])
-
-//     parentTaskObject = viewAll.projects[checkBox.closest('.project-container').dataset.id].tasks[parentTask.dataset.id]
-//     parentTaskObject.check()
-
-//     saveStorage(addProjectToStorage)
-
-// }
-
-// export {checkTask}
