0f003e61ffe70fda15400873e8182650ce803358
plzusername <MohamedFaris2210@gmail.com>
2024-03-14 16:07:57 +0300

    Add functionality to redoId's of task parent_project values

--- src/modules/Logic/redoIds.js
+++ src/modules/Logic/redoIds.js
@@ -1,7 +1,14 @@
-function redoIds(items, elements = undefined){
+function redoIds(items, elements = undefined, redoParentProject = false){
     if(items != 'none'){
         items.forEach((item, index) => {
             item.id = index
+        });
+    }
+    if(redoParentProject){
+        items.forEach((item, index) => {
+            item.tasks.forEach(task =>{
+                task.parent_project.id = index
+            })
         });
     }
     if(elements){
