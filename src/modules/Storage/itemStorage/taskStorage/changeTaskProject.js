cb4e8ae2952acc762143aab3918132cb59e5ec68
plzusername <MohamedFaris2210@gmail.com>
2024-03-15 15:31:35 +0300

    Clean up the function from unnecessary contents

--- src/modules/Storage/itemStorage/taskStorage/changeTaskProject.js
+++ src/modules/Storage/itemStorage/taskStorage/changeTaskProject.js
@@ -1,6 +1,5 @@
 import { viewAll } from "../../../Logic/view-all-projects"
 import { Storage, saveStorage } from "../../storage-utils/save-storage"
-import { projectGenerator } from "../../../Logic/project-Generator"
 
 function changeTaskProject(){
     Storage.projects = viewAll.projects
@@ -8,36 +7,15 @@ function changeTaskProject(){
 }
 
 function MoveItem(evt){
-    const movedTask = evt.item
     const taskNewPosition = evt.newIndex
     const taskOldPosition = evt.oldIndex
     const parentProject = viewAll.projects[evt.from.closest('.project-container').dataset.id]
     const newParentProject = viewAll.projects[evt.to.closest('.project-container').dataset.id]
     const taskToBeMoved = parentProject.tasks[taskOldPosition]
 
-    parentProject.changeProject(newParentProject, taskToBeMoved, taskNewPosition)
-
-
-    // const taskParentProjectObject = viewAll.projects[taskParentProject.dataset.id]
-    
+    parentProject.changeProject(newParentProject, taskToBeMoved, taskNewPosition)    
 
     saveStorage(changeTaskProject)
 
-    // const parentProject = document.querySelector(`.project-container[data-id="${window.currentTaskForm}"]`).querySelector('.task-parent-container')
-
-    // const parentProjectObject = viewAll.projects[window.currentTaskForm]
-
-    // parentProjectObject.addTask(generatedTask)
-
-    // saveStorage(addTaskToProjectStorage)
-
-    // titleInput.value = ''
-    // descriptionInput.value = ''
-    // dueDateInput.value = null
-    // priorityInput.selectedIndex = 0
-
-    // parentProject.appendChild(createTask(generatedTask))
-
 }
-
 export {MoveItem}
